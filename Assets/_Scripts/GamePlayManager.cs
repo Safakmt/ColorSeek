e9d1c866846c922624bd313c110aa48a1ffd8570
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-04 15:32:52 +0300

    Update GamePlayManager.cs

--- Assets/_Scripts/GamePlayManager.cs
+++ Assets/_Scripts/GamePlayManager.cs
@@ -18,12 +18,14 @@ public class GamePlayManager : MonoBehaviour
     public CinemachineBrain cinemachineBrain;
     public CinemachineVirtualCamera StartCam;
     public CinemachineVirtualCamera PlayCam;
+    public CinemachineVirtualCamera HunterCam;
 
     private void Start()
     {
         CurrentGameState = GameState.Start;
         StartCam.Priority = 1;
         PlayCam.Priority = 0;
+        HunterCam.Priority = 0;
     }
     private void Update()
     {
@@ -33,6 +35,11 @@ public class GamePlayManager : MonoBehaviour
             StartCam.Priority = 0;
             PlayCam.Priority = 1 ;
             OnGameStart?.Invoke();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            PlayCam.Priority = 0;
+            HunterCam.Priority= 1;
         }
     }
 }
