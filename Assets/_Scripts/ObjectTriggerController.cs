1d17072d079f6c1721285de45815ae61bad81387
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-05 21:14:45 +0300

    Update ObjectTriggerController.cs

--- Assets/_Scripts/ObjectTriggerController.cs
+++ Assets/_Scripts/ObjectTriggerController.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class ObjectTriggerController : MonoBehaviour
 {
+    private const string _validLayer = "Sticker";
     [SerializeField] private Transform stickingPoint;
-    public ISticker currentSticker;
+    private HideController currentHider;
     public bool isFree = true;
+    private int _layerIndex;
+    private void Awake()
+    {
+        _layerIndex = LayerMask.NameToLayer(_validLayer);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<ISticker>(out ISticker sticker) && isFree)
+        if (other.gameObject.layer == _layerIndex)
         {
-            currentSticker = sticker;
-            currentSticker.SetStickPoint(stickingPoint);
-            isFree = false;
+            if (isFree)
+            {
+                currentHider = other.GetComponent<HideController>();
+                currentHider.SetHidingSpot(stickingPoint);
+                isFree = false;
+            }
         }
-
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.TryGetComponent<ISticker>(out ISticker sticker) && sticker == currentSticker)
-        {
-            currentSticker.ClearStickPoint();
-            isFree = true;
-            currentSticker = null;
+        if (other.gameObject.layer == _layerIndex) { 
+
+            if (other.GetComponent<HideController>() == currentHider)
+            {
+                currentHider.ClearHidingSpot();
+                isFree = true;
+                currentHider = null;
+            }
+        
         }
     }
 }
