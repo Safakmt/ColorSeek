e582afe1e42a091583d47069be6a6c2629fa0431
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-06 21:30:03 +0300

    Create AnimatorController.cs

---
+++ Assets/_Scripts/AnimatorController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private const string RUN = "Run";
+    private const string IDLE = "Idle";
+    private const string TPOSE = "Tpose";
+    private string ActiveAnim;
+
+    public void PlayRunAnim()
+    {
+        _animator.SetBool(ActiveAnim, false);
+        _animator.SetBool(RUN, true);
+        ActiveAnim = RUN;
+    }
+    public void PlayIdleAnim()
+    {
+        _animator.SetBool(ActiveAnim, false);
+        _animator.SetBool(IDLE, true);
+        ActiveAnim = IDLE;
+    }
+    public void PlayTPoseAnim()
+    {
+        _animator.SetBool(ActiveAnim, false);
+        _animator.SetBool(TPOSE, true);
+        ActiveAnim = TPOSE;
+    }
+
+}
