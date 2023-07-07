e7d23c732a6846a2e35579dfff50b4e6876530ee
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-07 20:21:45 +0300

    Update HunterMovementController.cs

--- Assets/_Scripts/HunterMovementController.cs
+++ Assets/_Scripts/HunterMovementController.cs
@@ -13,6 +13,7 @@ public class HunterMovementController : MonoBehaviour
     [SerializeField] private int seekingCount;
     private List<HideController> _hidingList = new List<HideController>();
     private List<GameObject> _seekedList = new List<GameObject>();
+    private Transform currentChasedTransform;
     private void Start()
     {
         foreach (var item in hidingList)
@@ -21,14 +22,21 @@ public class HunterMovementController : MonoBehaviour
         }
         StartCoroutine(Seek());
     }
-    
+
+    private void Update()
+    {
+        if (currentChasedTransform != null)
+        {
+            agent.destination = currentChasedTransform.position;
+        }
+    }
     IEnumerator Seek()
     {
         while (seekingCount > 0)
         {
             GameObject hiding = hidingList[Random.Range(0, hidingList.Count)];
             Debug.Log("walking");
-            agent.destination = hiding.transform.position;
+            currentChasedTransform = hiding.transform;
             yield return new WaitUntil(() => Vector3.Distance(transform.position,agent.destination) <= 2f);
             hidingList.Remove(hiding);
             _seekedList.Add(hiding);
