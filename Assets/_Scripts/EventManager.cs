8d77c3ac077a104c2d5ec54b364ba3c06f124afb
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-07 15:39:22 +0300

    Update EventManager.cs

--- Assets/_Scripts/EventManager.cs
+++ Assets/_Scripts/EventManager.cs
@@ -6,5 +6,8 @@ public static class EventManager
     public static event Action OnGameStart;
     public static void GameStart() => OnGameStart?.Invoke();
 
+    public static event Action OnPlayerHide;
+    public static void PlayerHide() => OnPlayerHide?.Invoke();
+
     public static event Action OnTriggerExit;
 }
