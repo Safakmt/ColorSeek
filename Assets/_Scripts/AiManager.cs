8fb0cf257975877e803e35c9b3124e44c3101a7e
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-10 20:44:35 +0300

    Update AiManager.cs

--- Assets/_Scripts/AiManager.cs
+++ Assets/_Scripts/AiManager.cs
@@ -6,15 +6,21 @@ public class AiManager : MonoBehaviour
 {
     [SerializeField] private List<Transform> stickyTransforms = new List<Transform>();
     private AiMovementController[] _aiMovementControllers;
+    private HidingSpot[] _hidingSpots;
     private List<Transform> usedTransforms= new List<Transform>();
     private void Start()
     {
         _aiMovementControllers = FindObjectsOfType<AiMovementController>();
+        _hidingSpots = FindObjectsOfType<HidingSpot>();
         SetNewPositionsForAIs();
     }
 
     private void SetNewPositionsForAIs()
     {
+        for (int i = 0; i < _hidingSpots.Length; i++)
+        {
+            stickyTransforms.Add(_hidingSpots[i].transform);
+        }
         if (usedTransforms.Count > 0)
         {
             stickyTransforms.AddRange(usedTransforms);
