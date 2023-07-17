a42ca805e1db3d2a90e22eba2a3787d55422a845
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-17 11:34:32 +0300

    Update HunterMovementController.cs

--- Assets/_Scripts/HunterMovementController.cs
+++ Assets/_Scripts/HunterMovementController.cs
@@ -2,47 +2,100 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.AI;
+using static Cinemachine.CinemachineOrbitalTransposer;
 
+public enum HunterState
+{
+    Walk,
+    Catch
+}
 public class HunterMovementController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private PlayerController player;
-    [SerializeField] private List<GameObject> hidingList;
     [SerializeField] private int seekingCount;
+    [SerializeField] private Transform _catchingSpot;
+    [SerializeField] private Transform _catchingObjectPosition;
+    [SerializeField] private Animator _animator;
     private List<HideController> _hidingList = new List<HideController>();
-    private List<GameObject> _seekedList = new List<GameObject>();
-    private Transform currentChasedTransform;
+    private List<HideController> _seekedList = new List<HideController>();
+    private HideController currentChased;
+    private HunterState _currentState;
     private void Start()
     {
-        foreach (var item in hidingList)
+        HideController[] controllers = FindObjectsOfType<HideController>();
+        foreach (HideController controller in controllers)
         {
-            _hidingList.Add(item.GetComponent<HideController>());
+            _hidingList.Add(controller);
         }
-        StartCoroutine(Seek());
+        _currentState = HunterState.Walk;
     }
 
     private void Update()
     {
-        if (currentChasedTransform != null)
+        if (_currentState == HunterState.Walk)
         {
-            agent.destination = currentChasedTransform.position;
+            WalkStateActivites();
+        }
+
+        if (_currentState == HunterState.Catch)
+        {
+            CatchStateActivities();
         }
     }
-    IEnumerator Seek()
+
+    void WalkStateActivites()
     {
-        while (seekingCount > 0)
+        if (currentChased == null && seekingCount > 0)
         {
-            GameObject hiding = hidingList[Random.Range(0, hidingList.Count)];
-            Debug.Log("walking");
-            currentChasedTransform = hiding.transform;
-            yield return new WaitUntil(() => Vector3.Distance(transform.position,agent.destination) <= 2f);
-            hidingList.Remove(hiding);
-            _seekedList.Add(hiding);
-            hiding.SetActive(false);
-            Debug.Log("found hiding one");
-            seekingCount = seekingCount - 1;
+            currentChased = _hidingList[Random.Range(0, _hidingList.Count)];
+            _animator.SetTrigger("Walk");
+        }
+        else if (currentChased != null)
+        {
+            agent.destination = currentChased.transform.position;
+            if (Vector3.Distance(_catchingSpot.position, currentChased.transform.position) <= 5f)
+            {
+                _currentState = HunterState.Catch;
+                _hidingList.Remove(currentChased);
+                _seekedList.Add(currentChased);
+            }
+        }
+        else if (currentChased != null)
+        {
+            _animator.SetTrigger("Idle");
+        }
+    }
+
+    void CatchStateActivities()
+    {
+        if (currentChased != null)
+        {
+            agent.SetDestination(transform.position);
+            transform.LookAt(currentChased.transform.position);
+            currentChased.transform.SetParent(_catchingObjectPosition);
+            currentChased.transform.localPosition = Vector3.zero;
+            _animator.SetTrigger("Catch");
+            GameObject deactive = currentChased.gameObject;
+            DOVirtual.DelayedCall(1f, () =>
+            {
+                _currentState = HunterState.Walk;
+                deactive.gameObject.SetActive(false);
+            });
+    
+        currentChased = null;
+        seekingCount -= 1;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (currentChased!=null)
+        {
+            Gizmos.DrawRay(_catchingSpot.position,currentChased.transform.position);
+
         }
     }
 }
