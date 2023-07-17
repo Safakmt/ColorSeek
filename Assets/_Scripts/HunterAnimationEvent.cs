a93e72664484c031a092ddc152dce6aea0fd34b8
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-17 19:10:36 +0300

    Create HunterAnimationEvent.cs

---
+++ Assets/_Scripts/HunterAnimationEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterAnimationEvent : MonoBehaviour
+{
+    void Catch()
+    {
+        EventManager.HunterCatch();
+    }
+}
