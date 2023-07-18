c2e025bf8cdeeb5165398e96c1e87c00d1c502bc
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-18 12:08:22 +0300

    Update MeshColorHandler.cs

--- Assets/_Scripts/MeshColorHandler.cs
+++ Assets/_Scripts/MeshColorHandler.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class MeshColorHandler : MonoBehaviour
 {
     [SerializeField] private SkinnedMeshRenderer meshRenderer;
+    [SerializeField] private Color hidingSpecularColor;
     [SerializeField] private float hidingOutlineWidth;
+    [SerializeField] private float colorChangeDuration;
     private float _unhidingOutlineWidth = 2f;
+    private Color _unhidingSpecularColor;
 
     private void Awake()
     {
         meshRenderer.sharedMaterial.SetFloat("_Outline", _unhidingOutlineWidth);
+        _unhidingSpecularColor = meshRenderer.sharedMaterial.GetColor("_SpecColor");
     }
     public void SetColor(Color color)
     {
@@ -18,16 +22,23 @@ public class MeshColorHandler : MonoBehaviour
     }
     public void HidingValues()
     {
-        _unhidingOutlineWidth = meshRenderer.sharedMaterial.GetFloat("_Outline");
-        meshRenderer.sharedMaterial.SetFloat("_Outline", hidingOutlineWidth);
+        meshRenderer.sharedMaterial.DOColor(hidingSpecularColor, "_SpecColor", colorChangeDuration);
+        meshRenderer.sharedMaterial.DOFloat(hidingOutlineWidth, "_Outline", colorChangeDuration);
     }
 
     public void UnhidingValues()
     {
-        meshRenderer.sharedMaterial.SetFloat("_Outline", _unhidingOutlineWidth);
+        meshRenderer.sharedMaterial.DOColor(_unhidingSpecularColor, "_SpecColor", colorChangeDuration);
+        meshRenderer.sharedMaterial.DOFloat(_unhidingOutlineWidth, "_Outline", colorChangeDuration);
     }
     public Color GetColor()
     {
         return meshRenderer.sharedMaterial.color;
+    }
+
+    private void OnDisable()
+    {
+        meshRenderer.sharedMaterial.SetFloat("_Outline", _unhidingOutlineWidth);
+        meshRenderer.sharedMaterial.SetColor("_SpecColor",_unhidingSpecularColor);
     }
 }
