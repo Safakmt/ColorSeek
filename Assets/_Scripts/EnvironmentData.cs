a1786712e0b15d08fe3d6d84e1a002601b2e0558
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-26 13:17:29 +0300

    Create EnvironmentData.cs

---
+++ Assets/_Scripts/EnvironmentData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum Environment
+{
+    kitchen,
+    childroom,
+    table,
+    tutorial
+}
+public class EnvironmentData : MonoBehaviour
+{
+    public Transform charSpawnPos;
+    public Transform hunterSpawnPos;
+    public Transform hunterCamPos;
+    public Environment environment;
+    public List<HidingSpot> hidingSpots;
+
+    private void Start()
+    {
+        GamePlayManager.Instance.OnEnvironmentLoaded(this);
+        Debug.Log(environment.ToString() + " Loaded");
+    }
+
+}
