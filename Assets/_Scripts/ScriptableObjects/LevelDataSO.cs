046b7b6172148a7df5180899db0902ce65542989
Safakmt <77243118+Safakmt@users.noreply.github.com>
2023-07-26 12:14:17 +0300

    Add LevelDataSOs and Folder

---
+++ Assets/_Scripts/ScriptableObjects/LevelDataSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "ScriptableObjects/Level")]
+public class LevelDataSO : ScriptableObject
+{
+    public Environment Environment;
+    public int PlayTime;
+
+}
