f026b2334e98d39de81382caab307eecc4dbe47d
Şafak Mert Turan <safakmert@windowslive.com>
2023-08-16 18:26:58 +0300

    Update Movement

--- Assets/_Scripts/CPI_Scripts/MouseLook.cs
+++ Assets/_Scripts/CPI_Scripts/MouseLook.cs
@@ -6,13 +6,21 @@ public class MouseLook : MonoBehaviour
 {
     [SerializeField] private float mouseSensitivy = 100f;
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private float _speed;
     float xRotation = 0f;
 
     private void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivy * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivy * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
+        Vector3 newForward = transform.forward;
+        newForward.y = 0;
+        Vector3 move = transform.right * x + newForward * z;
+        _controller.Move(move * _speed * Time.deltaTime);
         xRotation -= mouseY;
 
         xRotation = Mathf.Clamp(xRotation, -90, 90);
